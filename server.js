bf14c75206a7369edf1a0acf141ecd0c328bf4e0
saxdgtea <saxdgtea@gmail.com>
2025-11-09 15:06:25 +0300

    leader imge

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use("/api/", limiter);
 // CORS configuration
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "https://church-front-nine.vercel.app",
     credentials: true,
   })
 );
